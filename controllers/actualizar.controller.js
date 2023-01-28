f399fef71c5526b39cdabd02c95c7f5bb87ba317
Jmt0972 <marcelotavella03@gmail.com>
2023-01-28 14:52:27 -0300

    CRUD JS final

--- controllers/actualizar.controller.js
+++ controllers/actualizar.controller.js
@@ -2,7 +2,7 @@ import { clientServices } from "../service/client-service.js";
 
 const formulario = document.querySelector("[data-form]");
 
-const obtenerInformacion = () => {
+const obtenerInformacion = async() => {
     const url = new URL(window.location);
     const id = url.searchParams.get("id");
     if (id === null){
@@ -10,11 +10,20 @@ const obtenerInformacion = () => {
     }
     const nombre = document.querySelector("[data-nombre]")
     const email = document.querySelector("[data-email]")
-    console.log(nombre, email)
-    clientServices.detalleCliente(id).then((perfil) => {
+    try{
+    const perfil = await clientServices.detalleCliente("id")
+    if(perfil.nombre && perfil.email){
         nombre.value = perfil.nombre;
         email.value = perfil.email;
-    });
+    }else{
+        throw new Error();
+    }
+    
+    }catch(error){
+        console.log("Catch error - ", error);
+    }
+    
+ 
 }
 
 obtenerInformacion();
